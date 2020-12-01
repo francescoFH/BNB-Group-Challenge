5edf7da4182c71ce5c47f9b55d0051c62af78bef
Francesco <francescofacca@gmail.com>
2020-12-01 01:52:14 +0000

    Trying to store booking - work in progress

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra/base'
 require 'sinatra/flash'
 require './lib/user'
-require './database_connection_setup'
 require './lib/space'
+require './lib/booking'
+require './database_connection_setup'
 
 class MakersBnB < Sinatra::Base
   enable :sessions
@@ -12,6 +13,12 @@ class MakersBnB < Sinatra::Base
     set :public_folder, File.expand_path('../public', __FILE__)
     set :views        , File.expand_path('../views', __FILE__)
     set :root         , File.dirname(__FILE__)
+  end
+
+  helpers do
+    def current_space
+      @current_space ||= Space.find(id: session[:space_id])
+    end
   end
 
   before do
@@ -66,7 +73,7 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/spaces/available' do
-    session[:available] = Space.available(from_date: params[:from], to_date: params[:to])
+    session[:available] = Space.available(params['from'], params['to'])
     redirect '/spaces'
   end
 
@@ -91,10 +98,17 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/spaces/:id/booking' do
+    session[:space_id] = params[:id]
+    Booking.create(
+      space_name: current_space.name,
+      booking_date: params[:date],
+      total_price: current_space.price,
+      name: @user.name, email: @user.email)
     redirect '/spaces'
   end
 
   get '/requests' do
+    @requests = Booking.find(id: @user.id) 
     erb :requests
   end
 
--- lib/space.rb
+++ lib/space.rb
@@ -20,7 +20,8 @@ class Space
       VALUES('#{owner}', '#{name}', '#{description}', #{price}, '#{from_date}', '#{to_date}')
       RETURNING id, owner, name, description, price, from_date, to_date;")
 
-    Space.new(id: result[0]["id"], owner: result[0]["owner"],
+    Space.new(
+      id: result[0]["id"], owner: result[0]["owner"],
       name: result[0]["name"], description: result[0]["description"],
       price: result[0]["price"], from_date: result[0]["from_date"],
       to_date: result[0]["to_date"])
@@ -30,25 +31,36 @@ class Space
     result = DatabaseConnection.query('SELECT * FROM spaces;')
 
     result.map do |space|
-      Space.new(id: space['id'], owner: space['owner'],
+      Space.new(
+        id: space['id'], owner: space['owner'],
         name: space['name'], description: space['description'],
         price: space['price'], from_date: space['from_date'],
         to_date: space['to_date'])
     end
   end
 
-  def self.available(from_date:, to_date:)
-    if :from_date == nil
-      result = DatabaseConnection.query("SELECT * FROM spaces;")
-    else
-      result = DatabaseConnection.query("SELECT * FROM spaces WHERE from_date >= '#{from_date}' AND to_date <= '#{to_date}';")
+  def self.find(id:)
+    result = DatabaseConnection.query("SELECT * FROM spaces WHERE id = #{id};")
 
+    Space.new(
+      id: result[0]["id"], owner: result[0]["owner"],
+      name: result[0]["name"], description: result[0]["description"],
+      price: result[0]["price"], from_date: result[0]["from_date"],
+      to_date: result[0]["to_date"])
+  end
+
+  def self.available(from_date:, to_date:)
+    # if :from_date == ""
+    #   result = DatabaseConnection.query("SELECT * FROM spaces;")
+    # else
+      result = DatabaseConnection.query("SELECT * FROM spaces WHERE from_date >= '#{from_date}' AND to_date <= '#{to_date}';")
+    # end
       result.map do |space|
-        Space.new(id: space['id'], owner: space['owner'],
+        Space.new(
+          id: space['id'], owner: space['owner'],
           name: space['name'], description: space['description'],
           price: space['price'], from_date: space['from_date'],
           to_date: space['to_date'])
       end
-    end
   end
 end
